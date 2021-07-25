0da8c3e76ce271a3fec97c0a39703752dabcdc23
Dounia <douniadif@outlook.com>
2021-07-25 05:43:11 +0200

    deployment

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/fitlog-auth',
+    DATABSE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/fitlog-auth',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/fitlog-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
   }
