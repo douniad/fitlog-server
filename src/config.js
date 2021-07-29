35fcd9c37544f9b15a70356704dc49de27a36e8e
Dounia <douniadif@outlook.com>
2021-07-29 17:29:24 +0200

    fiex typo

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABSE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/fitlog-auth',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://dunder_mifflin@localhost/fitlog-auth',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin@localhost/fitlog-test',
     JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
